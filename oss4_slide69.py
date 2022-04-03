458b4519f40690572a1c346a2447e6b5095284ee
Christos Gogos <chgogos@gmail.com>
2022-04-04 01:52:30 +0300

    fixes

--- oss4_slide69.py
+++ oss4_slide69.py
@@ -1,8 +1,8 @@
 import numpy as np
 mylist = [1,2,3,4]
-mytuple = (5,6,7,8)
 array1 = np.array(mylist)
 print(array1,'\n',type(array1))
 
+mytuple = (5,6,7,8)
 array2 = np.array(mytuple)
 print(array2,'\n',type(array2))
